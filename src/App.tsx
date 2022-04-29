81a18813eb03a448898e1f6177889d2edde2d44b
H-Builder <high-builder@hotmail.com>
2022-04-28 22:56:24 -0700

    fix: exception handling for react dom render

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { lazy, useContext, Suspense, useState, useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { LocaleContext, LocaleProvider } from './context/LocaleContext';
+import {
+  WalletProvider,
+  getChainOptions,
+  WalletControllerChainOptions,
+} from '@terra-money/wallet-provider';
+import Layout from './pages/Layout';
+import Loader from './components/Loader';
+import store from './states';
+
+import UserService from './states/user/updater';
+
+// Lazy Loaded Pages
+const CheckoutPage = lazy(() => import('./pages/Checkout'));
+
+interface Props {
+  open: boolean;
+  onClose?: (data: any) => void;
+  onSuccess?: (data: any) => void;
+  onError?: (error: any) => void;
+}
+
+const App: React.FC<Props> = ({ open }) => {
+  const { locale } = useContext(LocaleContext);
+  const [chainOptions, setChainOptions] = useState<
+    WalletControllerChainOptions | undefined
+  >(undefined);
+
+  useEffect(() => {
+    getChainOptions().then((options) => setChainOptions(options));
+  }, []);
+
+  function Services() {
+    return (
+      <>
+        <UserService />
+      </>
+    );
+  }
+
+  function Main() {
+    return (
+      <Provider store={store}>
+        <Suspense fallback={<Loader />}>
+          <LocaleProvider lang={locale}>
+            <Layout>{open && <CheckoutPage />}</Layout>
+          </LocaleProvider>
+        </Suspense>
+        <Services />
+      </Provider>
+    );
+  }
+
+  return chainOptions ? (
+    <WalletProvider {...chainOptions} children={<Main />} />
+  ) : null;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,9 @@
-import { lazy, useContext, Suspense, useState, useEffect } from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { LocaleContext, LocaleProvider } from './context/LocaleContext';
-import {
-  WalletProvider,
-  getChainOptions,
-  WalletControllerChainOptions,
-} from '@terra-money/wallet-provider';
-import Layout from './pages/Layout';
-import Loader from './components/Loader';
-import store from './states';
-
-import UserService from './states/user/updater';
+import App from './App';
 
 // import * as Sentry from '@sentry/react';
 // import { Integrations } from '@sentry/tracing';
 // import { CaptureConsole } from '@sentry/integrations';
-
-// Lazy Loaded Pages
-const CheckoutPage = lazy(() => import('./pages/Checkout'));
-
-const Services = () => (
-  <>
-    <UserService />
-  </>
-);
-
-export default function App() {
-  const { locale } = useContext(LocaleContext);
-  const [chainOptions, setChainOptions] = useState<
-    WalletControllerChainOptions | undefined
-  >(undefined);
-
-  useEffect(() => {
-    getChainOptions().then((options) => setChainOptions(options));
-  }, []);
-
-  function Main() {
-    return (
-      <Provider store={store}>
-        <Suspense fallback={<Loader />}>
-          <LocaleProvider lang={locale}>
-            <Layout>
-              <CheckoutPage />
-            </Layout>
-          </LocaleProvider>
-        </Suspense>
-        <Services />
-      </Provider>
-    );
-  }
-
-  return chainOptions ? (
-    <WalletProvider {...chainOptions} children={<Main />} />
-  ) : null;
-}
 
 // TODO: -- Get Sentry DSN
 // Sentry.init({
@@ -71,4 +20,6 @@ export default function App() {
 // });
 
 const rootElement = document.getElementById('kado-stable-pay-widget');
-render(<App />, rootElement);
+rootElement && render(<App open />, rootElement);
+
+export default App;
